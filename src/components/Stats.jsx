4b51540095f18d4db68cebe2cd0ddaf1d27351b4
MoreRamon <maranormore@gmail.com>
2026-08-20 23:00:58 -0300

    agrega el diseño de las tarjetas de estadísticas

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,26 +1,41 @@
-const Stats = ({ characters, words, sentences, readingTime }) => {
+const Stats = ({ characters, words, sentences }) => {
   return (
     <section className="stats-section">
-      <div className="stat-card stat-card-purple">
+      <article className="stat-card stat-card-purple">
+        <div className="stat-decoration decoration-purple"></div>
+
         <span className="stat-number">
           {String(characters).padStart(2, "0")}
         </span>
-        <span className="stat-label">Total Characters</span>
-      </div>
 
-      <div className="stat-card stat-card-orange">
+        <span className="stat-label">
+          Total Characters
+        </span>
+      </article>
+
+      <article className="stat-card stat-card-orange">
+        <div className="stat-decoration decoration-orange"></div>
+
         <span className="stat-number">
           {String(words).padStart(2, "0")}
         </span>
-        <span className="stat-label">Word Count</span>
-      </div>
 
-      <div className="stat-card stat-card-red">
+        <span className="stat-label">
+          Word Count
+        </span>
+      </article>
+
+      <article className="stat-card stat-card-red">
+        <div className="stat-decoration decoration-red"></div>
+
         <span className="stat-number">
           {String(sentences).padStart(2, "0")}
         </span>
-        <span className="stat-label">Sentence Count</span>
-      </div>
+
+        <span className="stat-label">
+          Sentence Count
+        </span>
+      </article>
     </section>
   )
 }
